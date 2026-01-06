732f070980a81de915210d08f0a7be6db63b09f2
chayatrapedo <chayatrapedo@gmail.com>
2026-01-05 22:40:42 -0500

    Fix demo file - Rewrite to type format string directly instead of choosing from menu - Dynamically creates my_scanf() parameters based on initial format string input

--- demo_program.c
+++ demo_program.c
@@ -1,80 +1,307 @@
+#include "my_scanf.h"
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 
-// Forward declaration of my_scanf from my_scanf.c
-int my_scanf(const char *format, ...);
-
 #define DEMO_BUFFER_SIZE 256
+#define MAX_ARGS 10
+
+// Format specifier parser matching my_scanf's implementation
+typedef struct {
+    char type;
+    int field_width;
+    char length_mod[3];
+    int suppress;
+    int exclaim;
+} FormatSpec;
+
+int parse_format(const char *format, FormatSpec specs[], int max_specs) {
+    int count = 0;
+    const char *p = format;
+
+    while (*p && count < max_specs) {
+        if (*p == '%') {
+            p++;
+            if (*p == '%') {
+                p++;
+                continue;
+            }
+
+            specs[count].suppress = 0;
+            specs[count].exclaim = 0;
+            specs[count].field_width = 0;
+            specs[count].length_mod[0] = '\0';
+
+            if (*p == '*') {
+                specs[count].suppress = 1;
+                p++;
+            }
+
+            if (*p == '!') {
+                specs[count].exclaim = 1;
+                p++;
+            }
+
+            while (isdigit(*p)) {
+                specs[count].field_width = specs[count].field_width * 10 + (*p - '0');
+                p++;
+            }
+
+            if (*p == 'h') {
+                specs[count].length_mod[0] = 'h';
+                specs[count].length_mod[1] = '\0';
+                p++;
+            } else if (*p == 'L') {
+                specs[count].length_mod[0] = 'L';
+                specs[count].length_mod[1] = '\0';
+                p++;
+            } else if (*p == 'l') {
+                specs[count].length_mod[0] = 'l';
+                if (*(p + 1) == 'l') {
+                    specs[count].length_mod[1] = 'l';
+                    specs[count].length_mod[2] = '\0';
+                    p += 2;
+                } else {
+                    specs[count].length_mod[1] = '\0';
+                    p++;
+                }
+            }
+
+            if (*p) {
+                specs[count].type = *p;
+                count++;
+                p++;
+            }
+        } else {
+            p++;
+        }
+    }
+
+    return count;
+}
 
 int main(int argc, char *argv[]) {
     (void)argc;
     (void)argv;
 
     char format_str[DEMO_BUFFER_SIZE];
+    char input_line[DEMO_BUFFER_SIZE];
 
-    printf("Enter a format string: ");
-    fgets(format_str, sizeof(format_str), stdin);
+    printf("=== my_scanf Interactive Demo ===\n");
+    printf("Enter 'quit' or '-1' at any prompt to exit\n\n");
 
-    // Remove newline if present
-    size_t len = strlen(format_str);
-    if (len > 0 && format_str[len - 1] == '\n') {
-        format_str[len - 1] = '\0';
-    }
+    while (1) {
+        printf("Enter a format string for my_scanf: ");
 
-    printf("Format string: %s\n", format_str);
-    printf("Enter input: ");
+        if (fgets(format_str, sizeof(format_str), stdin) == NULL) {
+            printf("\nEOF encountered. Exiting.\n");
+            break;
+        }
 
-    // Allocate space for potential values
-    int int_val;
-    long long_val;
-    float float_val;
-    char string_val[DEMO_BUFFER_SIZE];
-    char char_val;
+        size_t len = strlen(format_str);
+        if (len > 0 && format_str[len - 1] == '\n') {
+            format_str[len - 1] = '\0';
+        }
 
-    // Try different combinations based on format
-    int result = 0;
-    if (strstr(format_str, "%d") && strstr(format_str, "%s")) {
-        result = my_scanf(format_str, &int_val, string_val);
-        if (result >= 1) printf("  int: %d\n", int_val);
-        if (result >= 2) printf("  string: %s\n", string_val);
-    } else if (strstr(format_str, "%d") && strstr(format_str, "%f")) {
-        result = my_scanf(format_str, &int_val, &float_val);
-        if (result >= 1) printf("  int: %d\n", int_val);
-        if (result >= 2) printf("  float: %f\n", float_val);
-    } else if (strstr(format_str, "%ld")) {
-        result = my_scanf(format_str, &long_val);
-        if (result >= 1) printf("  long: %ld\n", long_val);
-    } else if (strstr(format_str, "%f")) {
-        result = my_scanf(format_str, &float_val);
-        if (result >= 1) printf("  float: %f\n", float_val);
-    } else if (strstr(format_str, "%s")) {
-        result = my_scanf(format_str, string_val);
-        if (result >= 1) printf("  string: %s\n", string_val);
-    } else if (strstr(format_str, "%c")) {
-        result = my_scanf(format_str, &char_val);
-        if (result >= 1) printf("  char: %c\n", char_val);
-    } else if (strstr(format_str, "%d")) {
-        result = my_scanf(format_str, &int_val);
-        if (result >= 1) printf("  int: %d\n", int_val);
-    } else if (strstr(format_str, "%z")) {
-        result = my_scanf(format_str, string_val);
-        if (result >= 1) printf("  result: %s\n", string_val);
-    } else if (strstr(format_str, "%b")) {
-        result = my_scanf(format_str, &int_val);
-        if (result >= 1) printf("  binary: %d\n", int_val);
-    } else if (strstr(format_str, "%x")) {
-        result = my_scanf(format_str, &int_val);
-        if (result >= 1) printf("  hex: %d\n", int_val);
-    } else if (strstr(format_str, "%q")) {
-        result = my_scanf(format_str, string_val);
-        if (result >= 1) printf("  cipher: %s\n", string_val);
-    }
+        if (strcmp(format_str, "quit") == 0 || strcmp(format_str, "-1") == 0) {
+            printf("Exiting demo.\n");
+            break;
+        }
 
-    if (result == 0) {
-        printf("No valid input found\n");
-    } else if (result == -1) {
-        printf("EOF encountered\n");
+        if (strlen(format_str) == 0) {
+            printf("Empty format string. Try again.\n\n");
+            continue;
+        }
+
+        FormatSpec specs[MAX_ARGS];
+        int num_specs = parse_format(format_str, specs, MAX_ARGS);
+
+        printf("my_scanf(%s): ", format_str);
+
+        if (fgets(input_line, sizeof(input_line), stdin) == NULL) {
+            printf("\nEOF encountered. Exiting.\n");
+            break;
+        }
+
+        len = strlen(input_line);
+        if (len > 0 && input_line[len - 1] == '\n') {
+            input_line[len - 1] = '\0';
+        }
+
+        if (strcmp(input_line, "quit") == 0 || strcmp(input_line, "-1") == 0) {
+            printf("Exiting demo.\n");
+            break;
+        }
+
+        FILE *temp = tmpfile();
+        if (temp == NULL) {
+            fprintf(stderr, "Error creating temporary file\n");
+            continue;
+        }
+
+        fprintf(temp, "%s\n", input_line);
+        rewind(temp);
+
+        FILE *original_stdin = stdin;
+        stdin = temp;
+
+        // Allocate storage
+        void *args[MAX_ARGS];
+        int int_vals[MAX_ARGS] = {0};
+        long long_vals[MAX_ARGS] = {0};
+        long long longlong_vals[MAX_ARGS] = {0};
+        short short_vals[MAX_ARGS] = {0};
+        float float_vals[MAX_ARGS] = {0};
+        double double_vals[MAX_ARGS] = {0};
+        long double ldouble_vals[MAX_ARGS] = {0};
+        char string_vals[MAX_ARGS][DEMO_BUFFER_SIZE] = {{0}};
+        char char_vals[MAX_ARGS] = {0};
+
+        int arg_idx = 0;
+        for (int i = 0; i < num_specs; i++) {
+            if (specs[i].suppress) {
+                continue;
+            }
+
+            switch (specs[i].type) {
+                case 'd':
+                case 'i':
+                    if (strcmp(specs[i].length_mod, "ll") == 0) {
+                        args[arg_idx++] = &longlong_vals[i];
+                    } else if (strcmp(specs[i].length_mod, "l") == 0) {
+                        args[arg_idx++] = &long_vals[i];
+                    } else if (strcmp(specs[i].length_mod, "h") == 0) {
+                        args[arg_idx++] = &short_vals[i];
+                    } else {
+                        args[arg_idx++] = &int_vals[i];
+                    }
+                    break;
+
+                case 'f':
+                    if (strcmp(specs[i].length_mod, "L") == 0) {
+                        args[arg_idx++] = &ldouble_vals[i];
+                    } else if (strcmp(specs[i].length_mod, "l") == 0) {
+                        args[arg_idx++] = &double_vals[i];
+                    } else {
+                        args[arg_idx++] = &float_vals[i];
+                    }
+                    break;
+
+                case 's':
+                case 'z':
+                case 'q':
+                    args[arg_idx++] = string_vals[i];
+                    break;
+
+                case 'c':
+                    args[arg_idx++] = &char_vals[i];
+                    break;
+
+                case 'x':
+                case 'b':
+                    args[arg_idx++] = &int_vals[i];
+                    break;
+
+                default:
+                    args[arg_idx++] = &int_vals[i];
+                    break;
+            }
+        }
+
+        int result = 0;
+        switch (arg_idx) {
+            case 0: result = my_scanf(format_str); break;
+            case 1: result = my_scanf(format_str, args[0]); break;
+            case 2: result = my_scanf(format_str, args[0], args[1]); break;
+            case 3: result = my_scanf(format_str, args[0], args[1], args[2]); break;
+            case 4: result = my_scanf(format_str, args[0], args[1], args[2], args[3]); break;
+            case 5: result = my_scanf(format_str, args[0], args[1], args[2], args[3], args[4]); break;
+            case 6: result = my_scanf(format_str, args[0], args[1], args[2], args[3], args[4], args[5]); break;
+            case 7: result = my_scanf(format_str, args[0], args[1], args[2], args[3], args[4], args[5], args[6]); break;
+            case 8: result = my_scanf(format_str, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]); break;
+            case 9: result = my_scanf(format_str, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]); break;
+            case 10: result = my_scanf(format_str, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]); break;
+        }
+
+        stdin = original_stdin;
+        fclose(temp);
+
+        printf("---\n");
+        printf("Fields matched: %d\n", result);
+
+        if (result > 0) {
+            int assigned_idx = 0;
+            for (int i = 0; i < num_specs; i++) {
+                char mod_str[16] = "";
+                if (specs[i].suppress) strcat(mod_str, "*");
+                if (specs[i].exclaim) strcat(mod_str, "!");
+                if (specs[i].field_width > 0) {
+                    char width_buf[8];
+                    sprintf(width_buf, "%d", specs[i].field_width);
+                    strcat(mod_str, width_buf);
+                }
+                if (specs[i].length_mod[0]) strcat(mod_str, specs[i].length_mod);
+
+                if (specs[i].suppress) {
+                    printf("  Field %d (%%%s%c): (suppressed)\n", i+1, mod_str, specs[i].type);
+                    continue;
+                }
+
+                if (assigned_idx >= result) break;
+
+                switch (specs[i].type) {
+                    case 'd':
+                    case 'i':
+                        if (strcmp(specs[i].length_mod, "ll") == 0) {
+                            printf("  Field %d (%%%s%c): %lld\n", i+1, mod_str, specs[i].type, longlong_vals[i]);
+                        } else if (strcmp(specs[i].length_mod, "l") == 0) {
+                            printf("  Field %d (%%%s%c): %ld\n", i+1, mod_str, specs[i].type, long_vals[i]);
+                        } else if (strcmp(specs[i].length_mod, "h") == 0) {
+                            printf("  Field %d (%%%s%c): %hd\n", i+1, mod_str, specs[i].type, short_vals[i]);
+                        } else {
+                            printf("  Field %d (%%%s%c): %d\n", i+1, mod_str, specs[i].type, int_vals[i]);
+                        }
+                        break;
+
+                    case 'f':
+                        if (strcmp(specs[i].length_mod, "L") == 0) {
+                            printf("  Field %d (%%%s%c): %Lf\n", i+1, mod_str, specs[i].type, ldouble_vals[i]);
+                        } else if (strcmp(specs[i].length_mod, "l") == 0) {
+                            printf("  Field %d (%%%s%c): %lf\n", i+1, mod_str, specs[i].type, double_vals[i]);
+                        } else {
+                            printf("  Field %d (%%%s%c): %f\n", i+1, mod_str, specs[i].type, float_vals[i]);
+                        }
+                        break;
+
+                    case 's':
+                    case 'z':
+                    case 'q':
+                        printf("  Field %d (%%%s%c): %s\n", i+1, mod_str, specs[i].type, string_vals[i]);
+                        break;
+
+                    case 'c':
+                        printf("  Field %d (%%%s%c): '%c'\n", i+1, mod_str, specs[i].type, char_vals[i]);
+                        break;
+
+                    case 'x':
+                        printf("  Field %d (%%%s%c): 0x%x (%d)\n", i+1, mod_str, specs[i].type, int_vals[i], int_vals[i]);
+                        break;
+
+                    case 'b':
+                        printf("  Field %d (%%%s%c): %d (binary)\n", i+1, mod_str, specs[i].type, int_vals[i]);
+                        break;
+                }
+
+                assigned_idx++;
+            }
+        } else if (result == 0) {
+            printf("  No valid input matched\n");
+        } else if (result == -1) {
+            printf("  EOF during parsing\n");
+        }
+
+        printf("\n");
     }
 
     return 0;
